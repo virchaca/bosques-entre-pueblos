43177c7f5ce9424087e62e7a0616c7e4de997353
virchaca <virginia.alvarez82@gmail.com>
2023-12-14 17:37:05 +0100

    @queries fauna

--- web/src/components/pageComponents/Fauna.jsx
+++ web/src/components/pageComponents/Fauna.jsx
@@ -2,8 +2,8 @@ import { Link } from "react-router-dom";
 import { useState } from "react";
 
 const Fauna = () => {
-  const [showAves, setShowAves] = useState(false);
-  const [showMamiferos, setShowMamiferos] = useState(true);
+  const [showAves, setShowAves] = useState(true);
+  const [showMamiferos, setShowMamiferos] = useState(false);
   const [showAnfibios, setShowAnfibios] = useState(true);
   
 const handleClickAves = () => { 
@@ -25,11 +25,163 @@ const handleClickAnfibios = () => {
       </Link>
 
       <div className="faunaSection" id="faunaSection">
-        <h1 className="faunaTitle" id="faunaTitle"> Aves y otros animales</h1>
+        <h1 className="faunaTitle" id="faunaTitle"> Algunos animales de la sierra de Francia</h1>
+        
+        <button className="faunaTableTitle" onClick={handleClickMamiferos}>-Mamíferos-</button> 
 
-        <button className="faunaTableTitle" onClick={handleClickAves}>Aves</button>
+        <button className="faunaTableTitle" onClick={handleClickAnfibios}>-Anfibios y Reptiles-</button>   
+
+        <button className="faunaTableTitle" onClick={handleClickAves}>-Aves-</button>
+
+        <table className={`${showMamiferos ? 'hidden' : ''} tableFauna`}  border="1">
+          <span>MAMIFEROS</span>
+          <tr>
+            <th>Nombre Científico</th>
+            <th>Nombre Común</th>
+            <th>Nombre Científico</th>
+            <th>Nombre Común</th>
+            <th>Nombre Científico</th>
+            <th>Nombre Común</th>
+          </tr>
+          <tr>
+            <td>Murciélago de cueva</td>
+            <td>Miniopterus schreibersii</td>
+            <td>Ratón de campo</td>
+            <td>Apodemus sylvaticus</td>
+            <td>Lirón careto</td>
+            <td>Eliomys gliridae</td>
+          </tr>
+          <tr>
+            <td>Murciélago gran.herradura</td>
+            <td>Rhinolophus ferrumequinum</td>
+            <td>Nutria</td>
+            <td>Lutra lutra</td>
+            <td>Topillo de cabrera</td>
+            <td>Microtus cabrerae</td>
+          </tr>
+          <tr>
+            <td>Murciélago peq. herradura</td>
+            <td>Rhinolophus hipposideros</td>
+            <td>Corzo</td>
+            <td>Capreolus capreolus</td>
+            <td>Jabalí</td>
+            <td>Sus scrofa</td>
+          </tr>
+          <tr>
+            <td>Ciervo</td>
+            <td>Cervus elaphus</td>
+            <td>Liebre ibérica</td>
+            <td>Lepus granatensis</td>
+            <td>Conejo</td>
+            <td>Oryctolagus cuniculus</td>
+          </tr>
+          <tr>
+            <td>Zorro</td>
+            <td>Vulpes vulpes</td>
+            <td>Comadreja</td>
+            <td>Mustela nivalis</td>
+            <td>Garduña</td>
+            <td>Martes foina</td>
+          </tr>
+          <tr>
+            <td>Gineta</td>
+            <td>Genetta genetta</td>
+            <td>Turón</td>
+            <td>Mustela putorius</td>
+            <td>Cabra montés</td>
+            <td>Capra pyrenaica</td>
+          </tr>
+          <tr>
+            <td>Erizo europeo</td>
+            <td>Erinaceus europaeus</td>
+            <td>Topo ibérico</td>
+            <td>Talpa occidentalis</td>
+            <td>Musaraña gris</td>
+            <td>Crocidura russula</td>
+          </tr>
+          <tr>
+            <td>Tejón</td>
+            <td>Meles meles</td>
+          </tr>
+        </table>  
+       
+        {/* <button className="faunaTableTitle" onClick={handleClickAnfibios}>Anfibios y Reptiles de la sierra de Francia</button>     */}
+        <table className={`${showAnfibios ? 'hidden' : ''}  tableFauna`}  border="1">
+        <span>ANFIBIOS Y REPTILES</span>
+          <tr>
+            <th>Nombre Científico</th>
+            <th>Nombre Común</th>
+            <th>Nombre Científico</th>
+            <th>Nombre Común</th>
+            <th>Nombre Científico</th>
+            <th>Nombre Común</th>
+          </tr>
+          <tr>
+            <td>Gallipato</td>
+            <td>Pleurodeles waltl</td>
+            <td>Salamandra común</td>
+            <td>Salamandra salamandra</td>
+            <td>Tritón ibérico</td>
+            <td>Lissotriton boscai</td>
+          </tr>
+          <tr>
+            <td>Tritón jaspeado</td>
+            <td>Triturus marmoratus</td>
+            <td>Sapo partero ibérico</td>
+            <td>Alytes dickhilleni</td>
+            <td>Sapo de espuelas</td>
+            <td>Pelobates cultripes</td>
+          </tr>
+          <tr>
+            <td>Sapo común, escuerzo</td>
+            <td>Bufo bufo</td>
+            <td>Sapo corredor</td>
+            <td>Epidalea calamita</td>
+            <td>Ranita de San Antonio</td>
+            <td>Hyla arborea</td>
+          </tr>
+          <tr>
+            <td>Rana verde o común</td>
+            <td>Pelophylax perezi</td>
+            <td>Galápago europeo</td>
+            <td>Emys orbicularis</td>
+            <td>Galápago leproso</td>
+            <td>Mauremys leprosa</td>
+          </tr>
+          <tr>
+            <td>Culebrilla ciega</td>
+            <td>Blanus cinereus</td>
+            <td>Salamanquesa común</td>
+            <td>Tarentola mauritanica</td>
+            <td>Lagarto ocelado</td>
+            <td>Timon lepidus</td>
+          </tr>
+          <tr>
+            <td>Lagartija colilarga</td>
+            <td>Psammodromus algirus</td>
+            <td>Lagartija cenicienta</td>
+            <td>Psammodromus hispanicus</td>
+            <td>Lagartija ibérica</td>
+            <td>Podarcis hispanicus</td>
+          </tr>
+          <tr>
+            <td>Culebra lisa meridional</td>
+            <td>Coronella girondica</td>
+            <td>Culebra de escalera</td>
+            <td>Zamenis scalaris</td>
+            <td>Culebra bastarda</td>
+            <td>Malpolon monspessulanus</td>
+          </tr>
+          <tr>
+            <td>Culebra viperina</td>
+            <td>Natrix maura</td>
+          </tr>
+        </table>   
+
+        {/* <button className="faunaTableTitle" onClick={handleClickAves}>Aves</button> */}
         <section className={showAves? 'hidden' : ''} >
         <table className='tableFauna' border="1">
+        <span>AVES</span>
           <tr>
             <th>Nombre Común</th>
             <th>Nombre Científico</th>
@@ -177,151 +329,7 @@ const handleClickAnfibios = () => {
             <td>Accipiter nisus</td>
           </tr>
         </table>
-        </section>
-        <button className="faunaTableTitle" onClick={handleClickMamiferos}>Mamíferos de la sierra de Francia</button>    
-        <table className={`${showMamiferos ? 'hidden' : ''} tableFauna`}  border="1">
-          <tr>
-            <th>Nombre Científico</th>
-            <th>Nombre Común</th>
-            <th>Nombre Científico</th>
-            <th>Nombre Común</th>
-            <th>Nombre Científico</th>
-            <th>Nombre Común</th>
-          </tr>
-          <tr>
-            <td>Murciélago de cueva</td>
-            <td>Miniopterus schreibersii</td>
-            <td>Ratón de campo</td>
-            <td>Apodemus sylvaticus</td>
-            <td>Lirón careto</td>
-            <td>Eliomys gliridae</td>
-          </tr>
-          <tr>
-            <td>Murciélago gran.herradura</td>
-            <td>Rhinolophus ferrumequinum</td>
-            <td>Nutria</td>
-            <td>Lutra lutra</td>
-            <td>Topillo de cabrera</td>
-            <td>Microtus cabrerae</td>
-          </tr>
-          <tr>
-            <td>Murciélago peq. herradura</td>
-            <td>Rhinolophus hipposideros</td>
-            <td>Corzo</td>
-            <td>Capreolus capreolus</td>
-            <td>Jabalí</td>
-            <td>Sus scrofa</td>
-          </tr>
-          <tr>
-            <td>Ciervo</td>
-            <td>Cervus elaphus</td>
-            <td>Liebre ibérica</td>
-            <td>Lepus granatensis</td>
-            <td>Conejo</td>
-            <td>Oryctolagus cuniculus</td>
-          </tr>
-          <tr>
-            <td>Zorro</td>
-            <td>Vulpes vulpes</td>
-            <td>Comadreja</td>
-            <td>Mustela nivalis</td>
-            <td>Garduña</td>
-            <td>Martes foina</td>
-          </tr>
-          <tr>
-            <td>Gineta</td>
-            <td>Genetta genetta</td>
-            <td>Turón</td>
-            <td>Mustela putorius</td>
-            <td>Cabra montés</td>
-            <td>Capra pyrenaica</td>
-          </tr>
-          <tr>
-            <td>Erizo europeo</td>
-            <td>Erinaceus europaeus</td>
-            <td>Topo ibérico</td>
-            <td>Talpa occidentalis</td>
-            <td>Musaraña gris</td>
-            <td>Crocidura russula</td>
-          </tr>
-          <tr>
-            <td>Tejón</td>
-            <td>Meles meles</td>
-          </tr>
-        </table>
-  
-       
-        <button className="faunaTableTitle" onClick={handleClickAnfibios}>Anfibios y Reptiles de la sierra de Francia</button>    
-        <table className={`${showAnfibios ? 'hidden' : ''}  tableFauna`}  border="1">
-          <tr>
-            <th>Nombre Científico</th>
-            <th>Nombre Común</th>
-            <th>Nombre Científico</th>
-            <th>Nombre Común</th>
-            <th>Nombre Científico</th>
-            <th>Nombre Común</th>
-          </tr>
-          <tr>
-            <td>Gallipato</td>
-            <td>Pleurodeles waltl</td>
-            <td>Salamandra común</td>
-            <td>Salamandra salamandra</td>
-            <td>Tritón ibérico</td>
-            <td>Lissotriton boscai</td>
-          </tr>
-          <tr>
-            <td>Tritón jaspeado</td>
-            <td>Triturus marmoratus</td>
-            <td>Sapo partero ibérico</td>
-            <td>Alytes dickhilleni</td>
-            <td>Sapo de espuelas</td>
-            <td>Pelobates cultripes</td>
-          </tr>
-          <tr>
-            <td>Sapo común, escuerzo</td>
-            <td>Bufo bufo</td>
-            <td>Sapo corredor</td>
-            <td>Epidalea calamita</td>
-            <td>Ranita de San Antonio</td>
-            <td>Hyla arborea</td>
-          </tr>
-          <tr>
-            <td>Rana verde o común</td>
-            <td>Pelophylax perezi</td>
-            <td>Galápago europeo</td>
-            <td>Emys orbicularis</td>
-            <td>Galápago leproso</td>
-            <td>Mauremys leprosa</td>
-          </tr>
-          <tr>
-            <td>Culebrilla ciega</td>
-            <td>Blanus cinereus</td>
-            <td>Salamanquesa común</td>
-            <td>Tarentola mauritanica</td>
-            <td>Lagarto ocelado</td>
-            <td>Timon lepidus</td>
-          </tr>
-          <tr>
-            <td>Lagartija colilarga</td>
-            <td>Psammodromus algirus</td>
-            <td>Lagartija cenicienta</td>
-            <td>Psammodromus hispanicus</td>
-            <td>Lagartija ibérica</td>
-            <td>Podarcis hispanicus</td>
-          </tr>
-          <tr>
-            <td>Culebra lisa meridional</td>
-            <td>Coronella girondica</td>
-            <td>Culebra de escalera</td>
-            <td>Zamenis scalaris</td>
-            <td>Culebra bastarda</td>
-            <td>Malpolon monspessulanus</td>
-          </tr>
-          <tr>
-            <td>Culebra viperina</td>
-            <td>Natrix maura</td>
-          </tr>
-        </table>      
+        </section>   
       </div>
       
       <Link to="/" className="backToMain">
